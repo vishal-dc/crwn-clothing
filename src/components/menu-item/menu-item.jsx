033590324a1b4d4447fbca6261d41a4dec7f909b
Vishal Chaudhari <vichaudh@tibco.com>
2021-04-10 20:56:30 +0530

    updated cart utils to group cart items

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -3,7 +3,8 @@ import './menu-item.styles.scss'
 import {withRouter} from 'react-router-dom'
 
 const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
-    <div className={`${size} menu-item`} onClick={()=> history.push(`${match.url}/shop`)}>
+    <div className={`${size} menu-item`}
+         onClick={()=> history.push(`${match.url}${linkUrl}`)}>
         <div style={{
             backgroundImage: `url(${imageUrl})`
         }} className='background-image'>
--- src/components/preview-collection/collection-preview.jsx
+++ src/components/preview-collection/collection-preview.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import './collection-preview.styles.scss'
+import './collection-preview.styles.scss';
 import CollectionItem from "../collection-item/collection-item";
 
 const CollectionPreview = ({title, items}) => (
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {items.filter((item,idx)=> idx<4).map(({id, ...otherProps}) => (
-                        <CollectionItem key={id} {...otherProps}/>
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+            {items.filter((item, idx) => idx < 4).map((item) => (
+                <CollectionItem key={item.id} item={item}/>
 
-                    ))}
+            ))}
 
         </div>
     </div>
-)
+);
 
-export default CollectionPreview
+export default CollectionPreview;
